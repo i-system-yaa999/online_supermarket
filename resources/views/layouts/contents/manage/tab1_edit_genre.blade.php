e25e43559c6e98f6dec9619bb4259ffbe4bb2ca6
takaya <ufkq56586@mineo.jp>
2022-07-29 13:09:07 +0900

    #22_add_manage_validation

--- resources/views/layouts/contents/manage/tab1_edit_genre.blade.php
+++ resources/views/layouts/contents/manage/tab1_edit_genre.blade.php
@@ -15,57 +15,48 @@
     <table class="tbl_item_list">
       <thead class="tbl_head">
         <tr>
-          {{-- <th></th> --}}
           <th class="list_center list_id">ID</th>
           <th class="fixed_head">売り場名</th>
           <th>作成日<br>------<br>更新日</th>
           <th></th>
           <th></th>
-          {{-- <th></th> --}}
         </tr>
       </thead>
       <tbody>
         @foreach($genres as $genre)
-        <tr class="@if($loop->iteration % 2) tbl_odd @else tbl_even @endif" id="tbl-item{{$genre->id}}">
-          {{-- チェックボックス --}}
-          {{-- <td class="list_center list_checkbox">
-            <input type="checkbox" name="" id="">
-          </td> --}}
-          {{-- id --}}
-          <td class="list_center list_id" name="genre_id{{$genre->id}}" id="genre_id{{$genre->id}}">
-            {{$genre->id}}
-          </td>
-          {{-- 売り場名 --}}
-          <td class="list_name @if($loop->iteration % 2) fixed_odd @else fixed_even @endif">
-            <input type="text" name="genre_name{{$genre->id}}" id="genre_name{{$genre->id}}" class="inputbox" value="{{$genre->name}}">
-            @if(($genre->id==old('genre_id')) && ($errors->has('genre_name')))
-            <div class="error_disp">{{$errors->first('genre_name')}}</div>
-            @endif
-          </td>
-          {{-- 作成日/更新日 --}}
-          <td class="list_created">{{$genre->created_at}}<span class="hr"></span>{{$genre->updated_at}}</td>
-          {{-- 登録ボタン --}}
-          <td class="list_center list_modify">
-            <form action="/manage" method="POST">
-              @method('PUT')
-              @csrf
-              <input type="hidden" value="{{$genre}}">
-              <input type="hidden" value="{{$genre->id}}">
-              <input type="hidden" value="{{$genre->name}}">
+        <tr class="@if($loop->iteration % 2) tbl_odd @else tbl_even @endif">
+          <form action="/manage" method="POST">
+            @method('PUT')
+            @csrf
+            <input type="hidden" name="tab_item" value="{{$tab_item}}">
+            {{-- id --}}
+            <td class="list_id">
+              <input type="text" name="genre_id" class="list_center list_id" value="{{$genre->id}}">
+            </td>
+            {{-- 売り場名 --}}
+            <td class="list_name @if($loop->iteration % 2) fixed_odd @else fixed_even @endif">
+              <input type="text" name="genre_name" class="inputbox" value="{{$genre->name}}">
+              @if(($genre->id==old('genre_id')) && ($errors->has('genre_name')))
+              <div class="error_disp">{{$errors->first('genre_name')}}</div>
+              @endif
+            </td>
+            {{-- 作成日/更新日 --}}
+            <td class="list_created">{{$genre->created_at}}<span class="hr"></span>{{$genre->updated_at}}</td>
+            {{-- 登録ボタン --}}
+            <td class="list_center list_modify">
               <button class="btn btn-modify" type="submit">登録</button>
-            </form>
-          </td>
+            </td>
+          </form>
           {{-- 削除ボタン --}}
           <td class="list_center list_delete">
             <form action="/manage" method="POST">
               @method('DELETE')
               @csrf
-              <input type="hidden" value="{{$genre->id}}">
+              <input type="hidden" name="tab_item" value="{{$tab_item}}">
+              <input type="hidden" name="genre_id" value="{{$genre->id}}">
               <button class="btn btn-delete" type="submit">削除</button>
             </form>
           </td>
-          {{-- 終端 --}}
-          {{-- <td class="list_terminal"></td> --}}
         </tr>
         @endforeach
       </tbody>
--- resources/views/layouts/contents/manage/tab3_edit_image.blade.php
+++ resources/views/layouts/contents/manage/tab3_edit_image.blade.php
@@ -15,7 +15,6 @@
     <table class="tbl_item_list">
       <thead>
         <tr class="tbl_head">
-          {{-- <th></th> --}}
           <th class="list_center list_id">ID</th>
           <th class="fixed_head">画像</th>
           <th>画像URL<br>image/products/</th>
@@ -23,58 +22,50 @@
           <th>作成日<br>------<br>更新日</th>
           <th></th>
           <th></th>
-          {{-- <th></th> --}}
         </tr>
       </thead>
       <tbody>
         @foreach($images as $image)
-        <tr class="@if($loop->iteration % 2) tbl_odd @else tbl_even @endif" id="tbl-item{{$image->id}}">
-          {{-- チェックボックス --}}
-          {{-- <td class="list_center list_checkbox">
-            <input type="checkbox" name="" id="">
-          </td> --}}
-          {{-- id --}}
-          <td class="list_center list_id" name="image_id{{$image->id}}" id="image_id{{$image->id}}">
-            {{$image->id}}
-          </td>
-          {{-- イメージurl --}}
-          <td class="list_image @if($loop->iteration % 2) fixed_odd @else fixed_even @endif">
-            <img src="{{$image->url}}" alt="">
-          </td>
-          <td class="list_imageURL">
-            <input type="text" name="image_url{{$image->url}}" id="image_url{{$image->id}}" class="inputbox" value="{{str_replace('image/products/', '', $image->url)}}">
-            @if(($image->id==old('image_id')) && ($errors->has('image_url')))
-            <div class="error_disp">{{$errors->first('image_url')}}</div>
-            @endif
-          </td>
-          {{-- 変更ボタン --}}
-          <td class="list_center list_upload">
-            <button class="btn btn-upload">画像変更</button>
-          </td>
-          {{-- 作成日/更新日 --}}
-          <td class="list_created">{{$image->created_at}}<span class="hr"></span>{{$image->updated_at}}</td>
-          {{-- 登録ボタン --}}
-          <td class="list_center list_modify">
-            <form action="/manage" method="POST">
-              @method('PUT')
-              @csrf
-              <input type="hidden" value="{{$image}}">
-              <input type="hidden" value="{{$image->id}}">
-              <input type="hidden" value="{{$image->url}}">
+        <tr class="@if($loop->iteration % 2) tbl_odd @else tbl_even @endif">
+          <form action="/manage" method="POST">
+            @method('PUT')
+            @csrf
+            <input type="hidden" name="tab_item" value="{{$tab_item}}">
+            {{-- id --}}
+            <td class="list_id">
+              <input type="text" name="image_id" class="list_center list_id" value="{{$image->id}}">
+            </td>
+            {{-- イメージurl --}}
+            <td class="list_image @if($loop->iteration % 2) fixed_odd @else fixed_even @endif">
+              <img src="{{$image->url}}" alt="">
+            </td>
+            <td class="list_imageURL">
+              <input type="text" name="image_url" class="inputbox" value="{{str_replace('image/products/', '', $image->url)}}">
+              @if(($image->id==old('image_id')) && ($errors->has('image_url')))
+              <div class="error_disp">{{$errors->first('image_url')}}</div>
+              @endif
+            </td>
+            {{-- 変更ボタン --}}
+            <td class="list_center list_upload">
+              <button class="btn btn-upload">画像変更</button>
+            </td>
+            {{-- 作成日/更新日 --}}
+            <td class="list_created">{{$image->created_at}}<span class="hr"></span>{{$image->updated_at}}</td>
+            {{-- 登録ボタン --}}
+            <td class="list_center list_modify">
               <button class="btn btn-modify" type="submit">登録</button>
-            </form>
-          </td>
+            </td>
+          </form>
           {{-- 削除ボタン --}}
           <td class="list_center list_delete">
             <form action="/manage" method="POST">
               @method('DELETE')
               @csrf
-              <input type="hidden" value="{{$image->id}}">
+              <input type="hidden" name="tab_item" value="{{$tab_item}}">
+              <input type="hidden" name="image_id" value="{{$image->id}}">
               <button class="btn btn-delete" type="submit">削除</button>
             </form>
           </td>
-          {{-- 終端 --}}
-          {{-- <td class="list_terminal"></td> --}}
         </tr>
         @endforeach
       </tbody>
